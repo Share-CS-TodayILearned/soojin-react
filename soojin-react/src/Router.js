c4a365cd624d90e6137d09830c1e8034f4ae95ca
정수진 <soojinjung@jeongsujin-ui-MacBookPro.local>
2022-06-20 15:41:02 +0900

    Add gradient colors

--- soojin-react/src/Router.js
+++ soojin-react/src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Toast from './pages/Toast';
+import Gradiant from './pages/Gradiant';
 
 const Router = () => {
     return (
@@ -9,6 +10,7 @@ const Router = () => {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/toast' element={<Toast />} />
+                <Route path='/gradiant' element={<Gradiant />} />
             </Routes>
         </BrowserRouter>
     )
