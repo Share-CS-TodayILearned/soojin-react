f1f521662494108863cbdc9e49a12a1fd714f30f
soojjung <sojjung3@gmail.com>
2022-06-26 11:21:28 +0900

    map에돌릴 id와 설명주석 추가

--- soojin-react/src/pages/Gradiant.js
+++ soojin-react/src/pages/Gradiant.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Gradiant = () => {
   return (
-      <>
+    <>
       <DivFlex>
         <Div11 />
         <Div2 />
         <Div3 />
         <Div4 />
         <Div5 />
-      </DivFlex> 
+      </DivFlex>
       <DivFlex>
         <Div6 />
         <Div7 />
@@ -19,121 +19,131 @@ const Gradiant = () => {
         <Div10 />
       </DivFlex>
       {/* <Div11 /> */}
-      </>
-  )
-}
+    </>
+  );
+};
 
 export default Gradiant;
 
 const DivFlex = styled.div`
-display : flex;
-`
-// 002 Night Fade
-const Div1 = styled.div`
-  margin : 20px;
-  height : 300px;
-  width : 300px;
-  border-radius: 50%;
-  background : yellow; 
-  background-image: linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%);
+  display: flex;
 `;
+// 002 Night Fade
+// const Div1 = styled.div`
+//   margin : 20px;
+//   height : 300px;
+//   width : 300px;
+//   border-radius: 50%;
+//   background : yellow;
+//   background-image: linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%);
+// `;
 
 // 010 Winter Neva
 const Div2 = styled.div`
-  margin : 20px;
-  height : 300px;
-  width : 300px;
+  margin: 20px;
+  height: 300px;
+  width: 300px;
   border-radius: 50%;
-  background : yellow; 
+  background: yellow;
   background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
 `;
 
 // 019 Malibu Beach
 const Div3 = styled.div`
-  margin : 20px;
-  height : 300px;
-  width : 300px;
+  margin: 20px;
+  height: 300px;
+  width: 300px;
   border-radius: 50%;
-  background : yellow; 
+  background: yellow;
   background-image: linear-gradient(to right, #4facfe 0%, #00f2fe 100%);
 `;
 
 // 030 Happy Fisher
 const Div4 = styled.div`
-  margin : 20px;
-  height : 300px;
-  width : 300px;
+  margin: 20px;
+  height: 300px;
+  width: 300px;
   border-radius: 50%;
-  background : yellow; 
+  background: yellow;
   background-image: linear-gradient(120deg, #89f7fe 0%, #66a6ff 100%);
 `;
 
 // 047 Fly High
 const Div5 = styled.div`
-  margin : 20px;
-  height : 300px;
-  width : 300px;
+  margin: 20px;
+  height: 300px;
+  width: 300px;
   border-radius: 50%;
-  background : yellow; 
+  background: yellow;
   background-image: linear-gradient(to top, #48c6ef 0%, #6f86d6 100%);
 `;
 
 // 051 February Ink
 const Div6 = styled.div`
-  margin : 20px;
-  height : 300px;
-  width : 300px;
+  margin: 20px;
+  height: 300px;
+  width: 300px;
   border-radius: 50%;
-  background : yellow; 
+  background: yellow;
   background-image: linear-gradient(to top, #accbee 0%, #e7f0fd 100%);
 `;
 
 // 106 Party Bliss
 const Div7 = styled.div`
-  margin : 20px;
-  height : 300px;
-  width : 300px;
+  margin: 20px;
+  height: 300px;
+  width: 300px;
   border-radius: 50%;
-  background : yellow; 
+  background: yellow;
   background-image: linear-gradient(to top, #4481eb 0%, #04befe 100%);
 `;
 
 // 109 River City
 const Div8 = styled.div`
-  margin : 20px;
-  height : 300px;
-  width : 300px;
+  margin: 20px;
+  height: 300px;
+  width: 300px;
   border-radius: 50%;
-  background : yellow; 
+  background: yellow;
   background-image: linear-gradient(to top, #4481eb 0%, #04befe 100%);
 `;
 
 // 120 Seashore
 const Div9 = styled.div`
-  margin : 20px;
-  height : 300px;
-  width : 300px;
+  margin: 20px;
+  height: 300px;
+  width: 300px;
   border-radius: 50%;
-  background : yellow; 
+  background: yellow;
   background-image: linear-gradient(to top, #209cff 0%, #68e0cf 100%);
 `;
 
 // 155 Landing Aircraft
 const Div10 = styled.div`
-  margin : 20px;
-  height : 300px;
-  width : 300px;
+  margin: 20px;
+  height: 300px;
+  width: 300px;
   border-radius: 50%;
-  background : yellow; 
-  background-image: linear-gradient(-225deg, #5D9FFF 0%, #B8DCFF 48%, #6BBBFF 100%);
+  background: yellow;
+  background-image: linear-gradient(
+    -225deg,
+    #5d9fff 0%,
+    #b8dcff 48%,
+    #6bbbff 100%
+  );
 `;
 
 // 159 Crystal River
 const Div11 = styled.div`
-  margin : 20px;
-  height : 300px;
-  width : 300px;
+  margin: 20px;
+  height: 300px;
+  width: 300px;
   border-radius: 50%;
-  background : yellow; 
-  background-image: linear-gradient(-225deg, #22E1FF 0%, #1D8FE1 48%, #625EB1 100%);
+  background: yellow;
+  background-image: linear-gradient(
+    -225deg,
+    #22e1ff 0%,
+    #1d8fe1 48%,
+    #625eb1 100%
+  );
 `;
--- soojin-react/src/pages/TripleReact/index.js
+++ soojin-react/src/pages/TripleReact/index.js
@@ -4,24 +4,22 @@ import DisplayValues from './DisplayValues/index.js';
 import { Wrapper } from './style.js';
 
 function TripleReact() {
+  // userCountArray에서 number는 api 통신으로 받는 데이터
+  // 백엔드에서 받기때문에 unique한 값을 가질것이고, 이를 id라 칭한다.
   const userCountArray = [
-    { number: 700, content: '만 명의 여행자' },
-    { number: 100, content: '만 개의 여행리뷰' },
-    { number: 470, content: '만 개의 여행일정' },
+    { id: 1, number: 700, content: '만 명의 여행자' },
+    { id: 2, number: 100, content: '만 개의 여행리뷰' },
+    { id: 3, number: 470, content: '만 개의 여행일정' },
   ];
 
   const animation = () => {
-    setTimeout(function () {
-      console.log('2초후에 한번 실행됩니다.');
-    }, 2000);
     const valueDisplays = document.querySelectorAll('.num');
-    // console.log(valueDisplays);
 
     valueDisplays.forEach(valueDisplay => {
-      // console.log(valueDisplay);
       let startValue = 0;
       const endValue = parseInt(valueDisplay.getAttribute('data-val'));
-      // console.log(endValue);
+      // duration은 애니매이션 동작 시간으로 2초로 고정한다.
+      // TODO : 2초로 동일하게 적용해도, 화면에서 동시에 끝나지 않는 부분 수정필요
       const duration = 2;
       const counter = setInterval(function () {
         startValue += 1;
@@ -33,15 +31,15 @@ function TripleReact() {
     });
   };
 
+  // 화면 맨처음 렌더링때만 애니매이션 필요하기때문에 useEffect에서 의존성배열을 []로 한다.
   useEffect(() => {
     animation();
   }, []);
 
   return (
     <Wrapper className="wrapper">
-      {userCountArray.map((userCount, index) => {
-        console.log('userCount', userCount);
-        return <DisplayValues userCount={userCount} />;
+      {userCountArray.map(userCount => {
+        return <DisplayValues key={userCount.id} userCount={userCount} />;
       })}
     </Wrapper>
   );
