e97cb89b8425a1fa403d404d78f0c732d6fce40f
정수진 <soojinjung@jeongsujin-ui-MacBookPro.local>
2022-06-28 09:53:49 +0900

    animation.js 사용없이

--- soojin-react/src/pages/TripleReact/hooks/useCountUp.js
+++ soojin-react/src/pages/TripleReact/hooks/useCountUp.js
@@ -1,20 +1,20 @@
 import { useEffect, useState } from 'react';
-import { easeInOutQuad } from '../utils/animation';
 
 const useCountUp = target => {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     const animationDuration = 2000;
-    const frameDuration = 2000 / 60;
-    const totalFrames = Math.round(animationDuration / frameDuration); // 60으로 constant한 값
+    const totalFrames = 60;
+    const frameDuration = animationDuration / totalFrames;
+
     let frame = 0;
 
     const counter = setInterval(() => {
-      frame++; // 0에서 60까지 증가한다
-      const progress = easeInOutQuad(frame / totalFrames);
-      console.log('progress', progress);
-      const currentCount = Math.round(target * progress);
+      frame++; // 1부터 60까지 증가한다
+      const currentCount = Math.round(target * (frame / totalFrames));
+      console.log('frame1111', target / totalFrames);
+      console.log('currentCount', currentCount);
 
       if (target >= currentCount) {
         setCount(currentCount);
