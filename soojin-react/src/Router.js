bf9d0881dd1968aa089480aeb4053e8103f9b39e
정수진 <soojinjung@jeongsujin-ui-MacBookPro.local>
2022-07-05 13:34:36 +0900

    Add BasicGrid

--- soojin-react/src/Router.js
+++ soojin-react/src/Router.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Toast from './pages/Toast';
+import BasicGrid from './pages/grid/BasicGrid';
 import Gradiant from './pages/Gradiant';
 import IncreaseNumber from './pages/IncreaseNumber';
 import TripleReact from './pages/TripleReact';
+import Toast from './pages/Toast';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/toast" element={<Toast />} />
+        <Route path="/grid" element={<BasicGrid />} />
         <Route path="/gradiant" element={<Gradiant />} />
         <Route path="/incnum" element={<IncreaseNumber />} />
         <Route path="/triplereact" element={<TripleReact />} />
+        <Route path="/toast" element={<Toast />} />
       </Routes>
     </BrowserRouter>
   );
